a053007ad26bdae076eb9c1175a667945c55e967
Pedro Henrique <pedro.phoa1405@gmail.com>
2024-05-26 21:15:21 -0300

    Avançando

--- src/main/java/br/fiap/cp3/Main.java
+++ src/main/java/br/fiap/cp3/Main.java
@@ -24,39 +24,39 @@ public class Main {
                 switch (cOption) {
                     case 1:
                         System.out.println("digite o nome do doador:");
-                        String nome = scanner.nextInt();
+                        String nome = scanner.nextString();
                         System.out.println("cpf do doador:");
-                        String cpf = scanner.nextInt();
+                        String cpf = scanner.nextLine();
                         System.out.println("endereco do doador:");
-                        String endereco = scanner.nextInt();
+                        String endereco = scanner.nextLine();
                         System.out.println("contato do doador:");
-                        String contato = scanner.nextInt();
+                        String contato = scanner.nextLine();
                         Doador.cadastrarDoador(nome,cpf,endereco,contato);
                     case 2:
                         System.out.println("digite o nome do voluntario:");
-                        String nome = scanner.nextInt();
+                        String nome = scanner.nextString();
                         System.out.println("cpf do voluntario:");
-                        String cpf = scanner.nextInt();
+                        String cpf = scanner.nextString();
                         System.out.println("endereco do voluntario:");
-                        String endereco = scanner.nextInt();
+                        String endereco = scanner.next();
                         System.out.println("contato do voluntrio:");
-                        String contato = scanner.nextInt();
+                        String contato = scanner.nextLine();
                         System.out.println("disponibilidade de horario:");
-                        String disp = scanner.nextInt();
+                        String disp = scanner.nextLine();
                         System.out.println("habilidades do voluntario:");
-                        String habilidades = scanner.nextInt();
+                        String habilidades = scanner.nextLine();
                         Voluntario.cadastrarVoluntario(nome,cpf,endereco,contato,disp,habilidades);
                     case 3:
                         System.out.println("digite o nome da instituição:");
-                        String nome = scanner.nextInt();
+                        String nome = scanner.nextLine();
                         System.out.println("cnpj da instituição:");
-                        String cnpj = scanner.nextInt();
+                        String cnpj = scanner.nextLine();
                         System.out.println("endereco da instituição:");
-                        String endereco = scanner.nextInt();
+                        String endereco = scanner.nextLine();
                         System.out.println("contato da instituição:");
-                        String contato = scanner.nextInt();
+                        String contato = scanner.nextLine();
                         System.out.println("área de atuação:");
-                        String areaAtuacao = scanner.nextInt();
+                        String areaAtuacao = scanner.nextLine();
                         InstituicaoCaridade.cadastrarInstituicao(nome,cnpj,endereco,contato,areaAtuacao);
                     default:
                         System.out.println("Opção inválida!");
@@ -64,18 +64,21 @@ public class Main {
                 break;
             case 2:
                 System.out.println("digite o nome do doador:");
-                String nome = scanner.nextInt();
+                String nome = scanner.nextLine();
                 System.out.println("digite a qual instituição você deseja mandar:");
-                String intituicao = scanner.nextInt();
+                String instituicao = scanner.nextLine();
                 System.out.println("deseja doar um valor ou um material?");
-                String tipo = scanner.nextInt();
+                String tipo = scanner.nextLine();
                 if tipo == "valor" {
                     System.out.println("digite o valor:");
-                    int valor = scanner.nextInt();
+                    int valor = scanner.nextLine();
+                    System.out.println("digite a data da doação");
+                    String data = scanner.nextLine();
+                    Doacao.cadastrarDoacao(nome, instituicao, tipo, valor, data)
+                }else{
+                    System.out.println("Agende com a institução sua doação");
                 }
-                else {}
-                System.out.println("digite a data da doação");
-                String data = scanner.nextInt();
+
 
             }
 
--- src/main/java/br/fiap/cp3/models/Doacao.java
+++ src/main/java/br/fiap/cp3/models/Doacao.java
@@ -56,3 +56,37 @@ public class Doacao {
         this.data = data;
     }
 }
+
+public class DoacaoManager {
+    private Doacao[] doacoes;
+    private int quantidadeDoacoes;
+
+    public DoacaoManager(int tamanhoMaximo) {
+        this.doacoes = new Doacao[tamanhoMaximo];
+        this.quantidadeDoacoes = 0;
+    }
+
+    public void cadastrarDoacao(Doador doador, InstituicaoCaridade instituicao, String tipoDoacao, double valor, String data) {
+        if (quantidadeDoacoes < doacoes.length) {
+            Doacao novaDoacao = new Doacao(doador, instituicao, tipoDoacao, valor, data);
+            doacoes[quantidadeDoacoes] = novaDoacao;
+            doador.adicionarDoacao(novaDoacao);
+            quantidadeDoacoes++;
+        } else {
+            System.out.println("Capacidade máxima de doações atingida.");
+        }
+    }
+
+    public void cruzarDadosDoacaoComDoador(String cpf) {
+        for (Doacao doacao : doacoes) {
+            if (doacao != null && doacao.getDoador().getCpf().equals(cpf)) {
+                System.out.println("Doação feita por: " + doacao.getDoador().getNome());
+                System.out.println("Tipo de doação: " + doacao.getTipoDoacao());
+                System.out.println("Valor da doação: " + doacao.getValor());
+                System.out.println("Data da doação: " + doacao.getData());
+                return;
+            }
+        }
+        System.out.println("Nenhuma doação encontrada para o CPF fornecido.");
+    }
+}
